e7c34119583e61f7655d6165906ccaec9739e012
linikujp <linikujp@gmail.com>
2015-01-27 22:46:33 -0500

    working file

---
+++ developer/city and states/city_county_transfer.py
@@ -0,0 +1,48 @@
+"""
+    
+
+
+    <!-- http://dbpedia.org/resource/United_Sates -->
+
+    <owl:NamedIndividual rdf:about="http://dbpedia.org/resource/United_Sates">
+        <rdf:type rdf:resource="&obo;MEBDO_0000020"/>
+        <rdfs:label xml:lang="en">United_States</rdfs:label>
+    </owl:NamedIndividual>
+    
+
+
+    <!-- http://dbpedia.org/resource/Georgia_(U.S._state) -->
+
+    <owl:NamedIndividual rdf:about="http://dbpedia.org/resource/Georgia_(U.S._state)">
+        <rdf:type rdf:resource="&obo;MEBDO_0000011"/>
+        <rdfs:label xml:lang="en">Georgia_(U.S._state)</rdfs:label>
+    </owl:NamedIndividual>
+    
+
+
+    <!-- http://dbpedia.org/resource/Hephzibah,_Georgia -->
+
+    <owl:NamedIndividual rdf:about="http://dbpedia.org/resource/Hephzibah,_Georgia">
+        <rdf:type rdf:resource="&obo;MEBDO_0000010"/>
+        <rdfs:label xml:lang="en">Hephzibah,_Georgia</rdfs:label>
+        <obo:MEBDO_0000410 rdf:resource="&obo;MEBDO_0000412"/>
+    </owl:NamedIndividual>
+    
+
+
+    <!-- http://dbpedia.org/resource/Richmond_County,_Georgia -->
+
+    <owl:NamedIndividual rdf:about="http://dbpedia.org/resource/Richmond_County,_Georgia">
+        <rdf:type rdf:resource="&obo;MEBDO_0000015"/>
+        <rdfs:label xml:lang="en">Richmond_County,_Georgia</rdfs:label>
+    </owl:NamedIndividual>
+  
+"""
+
+
+    <!-- http://purl.obolibrary.org/obo/MEBDO_0000412 -->
+
+    <owl:NamedIndividual rdf:about="&obo;MEBDO_0000412">
+        <rdf:type rdf:resource="&obo;MEBDO_0000012"/>
+        <rdfs:label xml:lang="en">30815 zip code zone in 2012</rdfs:label>
+    </owl:NamedIndividual>"""
